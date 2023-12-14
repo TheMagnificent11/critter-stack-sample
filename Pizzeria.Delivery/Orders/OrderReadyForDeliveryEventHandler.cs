3e96917af58001aa2488a5cd52017f9ba5747a8c
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-12-15 00:12:36 +1000

    hope this works

---
+++ Pizzeria.Delivery/Orders/OrderReadyForDeliveryEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using Pizzeria.Domain;
+using Wolverine;
+
+namespace Pizzeria.Delivery.Orders;
+
+public class OrderReadyForDeliveryEventHandler(
+    IMessageBus messageBus,
+    ILogger<OrderReadyForDeliveryEventHandler> logger)
+{
+    public async Task Handle(OrderReadyForDeliveryEvent @event, CancellationToken cancellationToken)
+    {
+        // Wait a random amount of time between 5 and 10 seconds
+        var random = new Random();
+        var seconds = random.Next(5, 10);
+        await Task.Delay(TimeSpan.FromSeconds(seconds), cancellationToken);
+
+        logger.LogInformation("Order {OrderId} has been delivered", @event.OrderId);
+
+        await messageBus.PublishAsync(new OrderDeliveredEvent(@event.OrderId, @event.CorrelationId));
+    }
+}
---
+++ Pizzeria.Store/Orders/OrderDeliveredEventHandler.cs
@@ -0,0 +1,25 @@
+﻿using Marten;
+using Pizzeria.Domain;
+
+namespace Pizzeria.Store.Orders;
+
+public class OrderDeliveredEventHandler(IDocumentStore store, ILogger<OrderDeliveredEventHandler> logger)
+{
+    public async Task Handle(OrderDeliveredEvent @event, CancellationToken cancellationToken)
+    {
+        using (var session = store.LightweightSession())
+        {
+            var order = await session.LoadAsync<Order>(@event.OrderId, cancellationToken);
+
+            if (order == null)
+            {
+                logger.LogError("Order {OrderId} not found", @event.OrderId);
+                return;
+            }
+
+            order.PizzasDelivered();
+
+            await session.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
