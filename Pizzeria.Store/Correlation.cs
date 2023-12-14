3fb80bfc9a43c0f7c2bf7a7c417f7e0ccfc6ad27
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-12-15 02:37:28 +1000

    tidy-up

--- Pizzeria.Store/Correlation.cs
+++ Pizzeria.Store/Correlation.cs
@@ -4,7 +4,5 @@ public static class Correlation
 {
     public const string HeaderName = "X-Correlation-ID";
 
-#pragma warning disable SA1010 // Opening square brackets should be spaced correctly
-    public static readonly string[] RequestHeaders = [HeaderName];
-#pragma warning restore SA1010 // Opening square brackets should be spaced correctly
+    public static readonly string[] RequestHeaders = { HeaderName };
 }
