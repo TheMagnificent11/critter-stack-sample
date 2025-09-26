756729ee9f8bfe6e1356a47c6d17dd9271999fc1
Saji Weerasingham <saji.weerasingham@outlook.com>
2025-09-26 16:39:56 +1000

    tidy-up

--- Pizzeria.Common/ServiceNames.cs
+++ Pizzeria.Common/ServiceNames.cs
@@ -2,9 +2,9 @@ namespace Pizzeria.Common;
 
 public static class ServiceNames
 {
-    public const string DatabaseServer = "databaseServer";
-    public const string StoreDatabase = "storeDatabase";
-    public const string MessageBroker = "messageBroker";
+    public const string DatabaseServer = "database-server";
+    public const string StoreDatabase = "store-database";
+    public const string MessageBroker = "message-broker";
     public const string Logging = "seq";
     public const string Store = "pizzeria-store";
     public const string Kitchen = "pizzeria-kitchen";
